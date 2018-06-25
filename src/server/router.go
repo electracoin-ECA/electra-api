32b7e038baae4af74c0f1d05d25ce1c3db326743
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-25 13:28:05 +0200

    server/router: add /v1/price/:currency & /price/:currency

--- src/server/router.go
+++ src/server/router.go
@@ -1,15 +1,15 @@
 package server
 
 import (
-	"github.com/Electra-project/electra-auth/src/controllers"
-	"github.com/Electra-project/electra-auth/src/middlewares"
+	"github.com/Electra-project/electra-api/src/controllers"
+	"github.com/Electra-project/electra-api/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 // Router binds the routes to the controllers.
 func Router() *gin.Engine {
 	router := gin.Default()
-	router.Use(middlewares.Cors())
+	router.Use(middlewares.CORS())
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -18,6 +18,9 @@ func Router() *gin.Engine {
 		})
 	})
 
+	priceController := new(controllers.PriceController)
+	router.GET("/v1/price/:currency", priceController.Get) // Legacy route
+	router.GET("/price/:currency", priceController.Get)
 	statusController := new(controllers.StatusController)
 	router.GET("/status", statusController.Get)
 
