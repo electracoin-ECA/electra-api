4d9d203448bac8017e123c9e60f45ae3bf49505c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-25 12:55:38 +0200

    libs/rpc/rpc: create

---
+++ src/libs/rpc/rpc.go
@@ -0,0 +1,54 @@
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Electra-project/electra-auth/src/helpers"
+)
+
+func query(method string, params []string, response interface{}) error {
+	daemonURI := "http://127.0.0.1:5788"
+
+	reqParams, err := json.Marshal(params)
+	if err != nil {
+		helpers.LogErr("Error: " + err.Error())
+
+		return err
+	}
+
+	reqData := `{"method":"` + method + `","params":` + string(reqParams) + `}`
+	// helpers.Log(reqData)
+	reqDataBuffer := bytes.NewBuffer([]byte(reqData))
+	req, err := http.NewRequest("POST", daemonURI, reqDataBuffer)
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth("user", "pass")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		helpers.LogErr("Error: " + err.Error())
+
+		return err
+	}
+
+	defer res.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		helpers.LogErr("Error: " + err.Error())
+
+		return err
+	}
+
+	err = json.Unmarshal(bodyBytes, &response)
+	if err != nil {
+		helpers.LogErr("Error: " + err.Error())
+
+		return err
+	}
+
+	return nil
+}
