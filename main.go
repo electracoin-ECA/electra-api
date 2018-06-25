06b6998514bf13f031f61c9b41e9669d664e59ce
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-25 13:29:12 +0200

    main: fix import

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/Electra-project/electra-auth/src/helpers"
-	"github.com/Electra-project/electra-auth/src/server"
+	"github.com/Electra-project/electra-api/src/helpers"
+	"github.com/Electra-project/electra-api/src/server"
 	"github.com/joho/godotenv"
 )
 
