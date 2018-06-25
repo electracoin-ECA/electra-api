be31362b589fd77623d93ffa7af57c763d4cf788
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-25 12:56:30 +0200

    controllers/status: create

---
+++ src/controllers/status.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"github.com/Electra-project/electra-auth/src/libs/rpc"
+	"github.com/gin-gonic/gin"
+)
+
+// StatusController describes a status controller.
+type StatusController struct{}
+
+// Get a user public data.
+func (s StatusController) Get(c *gin.Context) {
+	status, err := rpc.GetInfo()
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Internal Server Error."})
+		c.Abort()
+
+		return
+	}
+
+	c.Header("X-Version", "1.0")
+	c.JSON(200, status)
+
+	return
+}
