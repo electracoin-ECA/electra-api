3a8f678fe0349949e6467517a5e674f6e2087f2a
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-25 12:56:37 +0200

    controllers/user: create

---
+++ src/controllers/user.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Electra-project/electra-auth/src/models"
+
+	"github.com/Electra-project/electra-auth/src/libs/fail"
+	"github.com/gin-gonic/gin"
+)
+
+// UserController describes a user controller.
+type UserController struct{}
+
+// Get retrieves the authenticated user data.
+func (u UserController) Get(c *gin.Context) {
+	purseHash := getPurseHash(c)
+
+	user, err := userModel.GetByPurseHash(purseHash)
+	if err != nil {
+		fail.Answer(c, err, "user")
+
+		return
+	}
+
+	c.Header("X-Version", "1.0")
+	c.JSON(http.StatusOK, gin.H{"data": user})
+
+	return
+}
+
+// Post creates a new database entry for the authenticated user.
+func (u UserController) Post(c *gin.Context) {
+	purseHash := getPurseHash(c)
+
+	user, err := userModel.Insert(purseHash)
+	if err != nil {
+		fail.Answer(c, err, "user")
+
+		return
+	}
+
+	c.Header("X-Version", "1.0")
+	c.JSON(http.StatusCreated, gin.H{"data": user})
+}
+
+// Put update the authenticated user data.
+func (u UserController) Put(c *gin.Context) {
+	purseHash := getPurseHash(c)
+
+	var data models.UserEditable
+	err := c.BindJSON(&data)
+	if err != nil {
+		fail.Answer(c, err, "user")
+
+		return
+	}
+
+	user, err := userModel.Update(purseHash, data)
+	if err != nil {
+		fail.Answer(c, err, "user")
+
+		return
+	}
+
+	c.Header("X-Version", "1.0")
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
