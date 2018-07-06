5bc9837f638ce960ccfaab33ecca02ed6b5746c4
Girish Ramnani <girishramnani95@gmail.com>
2018-07-06 15:29:03 +0530

    Added address functions and models

--- src/libs/electra-api/address.go
+++ src/libs/electra-api/address.go
@@ -1,5 +1,51 @@
 package api
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Electra-project/electra-api/src/helpers"
+	"github.com/Electra-project/electra-api/src/models"
+)
+
 // CoinbaseAddress is a constant which tells us that the address in the transaction is
 // done using coinbase. We want to avoid adding this to the database
 const CoinbaseAddress = "coinbase"
+
+const MaxReq = 5
+
+// GetAddressData gets the balance and other related information for the provided address
+func GetAddressData(address string) (*models.Address, error) {
+
+	var addr models.Address
+	errOccured := helpers.GetJSON(fmt.Sprintf("%s/getaddress/%s", ApiURLPrefix, address), &addr)
+
+	if errOccured {
+		return nil, errors.New(ErrMalformedResponseFromUpstreamAPI)
+	}
+
+	return &addr, nil
+
+}
+
+func GetAddressDatas(addresses ...string) (chan *models.Address, error) {
+	controlChan := make(chan bool, MaxReq)
+	defer close(controlChan)
+	controlChan = fillChannel(controlChan, MaxReq)
+	// we will handle closing the channel in parent function
+	addressChan := make(chan *models.Address, len(addresses))
+
+	for _, address := range addresses {
+		<-controlChan
+		go func() {
+			GetAddressData()
+		}
+	}
+}
+
+func fillChannel(c chan bool, factor int) chan bool {
+	for _i := 0; _i < factor; _i++ {
+		c <- true // doesn't matter if true or false
+	}
+	return c
+}
